e7797fb907dca918e4fe121ffeaab5c8498b0423
Rich Shea <richcshea@gmail.com>
2015-08-19 09:59:01 -0600

    update seed file

--- db/seeds.rb
+++ db/seeds.rb
@@ -15,6 +15,16 @@ def generate_resources
      name: "SQL Course",
      description: "Learn SQL with a series of basic exercises.",
      link: "http://sqlcourse.com"
+    },
+    {
+     name: "Vic Ramon's Ember Tutorial",
+     description: "Online tutorial for learning the Ember framework",
+     link: "http://ember.vicramon.com"
+    },
+    {
+     name: "Sitepoint - How to use jQuery's $.ajax() function",
+     description: "Online tutorial that provides detailed steps to use $.ajax() function",
+     link: "http://http://www.sitepoint.com/use-jquerys-ajax-function"
     }
   ])
   puts "resources complete."
@@ -23,6 +33,13 @@ end
 def generate_users
   puts "generating users..."
   User.create!([
+    {
+     name: "admin",
+     email: "admin@example.com",
+     password: "password",
+     password_confirmation: "password",
+     admin: true
+    },
     {
      name: "User1",
      email: "user1@example.com",
@@ -44,5 +61,20 @@ def generate_users
   puts "users complete."
 end
 
+def comments
+  ['The best resource ever!', 'Waste of time', 'I learned so much from this resource', 'Really challenging, but worth the effort']
+end
+
+def generate_reviews
+  puts "generating reviews..."
+    Resource.all.each do |resource|
+      User.all.each do |user|
+        Review.create!(resource: resource, user: user, comment: comments[rand(3)], rating: rand(0..5))
+      end
+    end
+  puts "reviews complete."
+end
+
 generate_resources
 generate_users
+generate_reviews
