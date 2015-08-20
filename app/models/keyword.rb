745905bedb61a078143df95bc561c21c26811bd1
Rich Shea <richcshea@gmail.com>
2015-08-20 16:26:48 -0600

    order resource filter alpha not-case-sensitvie

--- app/models/keyword.rb
+++ app/models/keyword.rb
@@ -1,4 +1,6 @@
 class Keyword < ActiveRecord::Base
   has_many :categorizations, dependent: :destroy
   has_many :resources, through: :categorizations
+
+  scope :order_by_name, -> {order('LOWER(name)')}
 end
