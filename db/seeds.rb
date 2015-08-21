141b571e375f0b27717ecf4d01c648a60833239b
Rich Shea <richcshea@gmail.com>
2015-08-21 11:37:38 -0600

    update the seed data for deployment

--- db/seeds.rb
+++ db/seeds.rb
@@ -24,57 +24,21 @@ def generate_resources
     {
      name: "Sitepoint - How to use jQuery's $.ajax() function",
      description: "Online tutorial that provides detailed steps to use $.ajax() function",
-     link: "http://http://www.sitepoint.com/use-jquerys-ajax-function"
+     link: "http://www.sitepoint.com/use-jquerys-ajax-function"
     }
   ])
   puts "resources complete."
 end
 
-def generate_users
-  puts "generating users..."
-  User.create!([
-    {
-     name: "admin",
-     email: "admin@example.com",
-     password: "password",
-     password_confirmation: "password",
-     admin: true
-    },
-    {
-     name: "User1",
-     email: "user1@example.com",
-     password: "password",
-     password_confirmation: "password"
-    },
-    {
-     name: "User2",
-     email: "user2@example.com",
-     password: "password",
-     password_confirmation: "password"
-    },
-    {
-     name: "User3",
-     email: "user3@example.com",
-     password: "password",
-     password_confirmation: "password"
-    }])
-  puts "users complete."
-end
-
-def comments
-  ['The best resource ever!', 'Waste of time', 'I learned so much from this resource', 'Really challenging, but worth the effort']
-end
-
-def generate_reviews
-  puts "generating reviews..."
-    Resource.all.each do |resource|
-      User.all.each do |user|
-        Review.create!(resource: resource, user: user, comment: comments[rand(3)], rating: rand(0..5))
-      end
-    end
-  puts "reviews complete."
+def generate_keywords
+  puts "generating keywords..."
+  words = %w(Rails Ruby JavaScript jQuery Ember HTML CSS RSpec AJAX Testing)
+  words.each do |word|
+    Keyword.create(name: word)
+    puts "Keyword: #{word}"
+  end
+  puts "keywords complete."
 end
 
 generate_resources
-generate_users
-generate_reviews
+generate_keywords
