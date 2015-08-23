b8cbd2b57a6784caacd96baf627c5eedd9be69c3
Rich Shea <richcshea@gmail.com>
2015-08-23 13:16:41 -0600

    add link for admin to view all users:

--- spec/features/list_users_spec.rb
+++ spec/features/list_users_spec.rb
@@ -5,14 +5,17 @@ describe "Listing users" do
     user1 = User.create(user_attributes(email: "user1@example.com"))
     user2 = User.create(user_attributes(email: "user2@example.com"))
     user3 = User.create(user_attributes(email: "user3@example.com"))
+    admin = User.create(user_attributes(email: "admin@example.com", admin: true))
 
     visit root_url
-    sign_in(user1)
+    sign_in(admin)
+    click_on("My Account")
+    click_on("All Users")
 
-    visit users_url
-
+    expect(current_path).to eq(users_path) 
     expect(page).to have_text(user1.name)
     expect(page).to have_text(user2.name)
     expect(page).to have_text(user3.name)
   end
+
 end
